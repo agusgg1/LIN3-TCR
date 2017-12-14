774cfdbcffe0e4ae43fc0f4a3e7bcffdd6bd6800
agusgg1 <dewaputugangga88@gmail.com>
2017-12-14 05:32:03 -0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -36,7 +36,7 @@ class LINE:
 
     self.authToken = self.Talk.authToken
     self.cert = self.Talk.cert
-    self._headers = {'X-Line-Application': 'IOSIPAD\t7.14.0\tiPhone OS\t10.12.0', 'X-Line-Access': self.authToken, 'User-Agent': 'Line/6.0.0 iPad4,1 9.0.2'}zz
+    self._headers = {'X-Line-Application': 'IOSIPAD\t7.14.0\tiPhone OS\t10.12.0', 'X-Line-Access': self.authToken, 'User-Agent': 'Line/6.0.0 iPad4,1 9.0.2'}
 
     self.Poll = Poll(self.authToken)
     self.channel = channel.Channel(self.authToken)
